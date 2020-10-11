f76777d644bf8f81db6495f4c6cac6280bd96240
unknown <ravibs1994@gmail.com>
2020-10-11 13:56:35 +0530

    CalculateDailyWage

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -x
-isFulltime=1
-randomCheck=$((RANDOM%2))
-if [ $randomCheck -eq $isFulltime ]
-then
-        empRatePerhour=20
-        emphrs=8
-        salary=$(($empRatePerhour*$emphrs))
-else
-        salary=0
-fi
-echo "Your one day salary is :" $salary
+read -p "ENTER THE NUMBER OF EMPLOY" NUM
+ISFULLTIME=1
+WAGES_PER_HOUR=20
+DAILY_EMPLOY_WAGES=0
+for (( i=1;i<=$NUM;i++ ))
+do
+	ATTEND=$((RANDOM%2))
+	if [ $ATTEND -eq 1 ]
+	then
+		DAILY_EMPLOY_WAGES=$((WAGES_PER_HOUR*8))
+		echo "WAGES OF EMPLOY $i:$DAILY_EMPLOY_WAGES"
+	else
+		echo "EMPLOY $i ABSENT"
+	fi
+done
 
